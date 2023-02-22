c489848b963cc49936d96f60ac973172fb400773
Cristy Gonzales <cristyg2025@gmail.com>
2023-02-21 18:17:38 -0800

    removed grid.py functionality

--- grid.py
+++ grid.py
@@ -2,10 +2,10 @@ import sys
 from PyQt6.QtWidgets import QApplication, QDialog, QGridLayout, QLabel, QLineEdit, QPushButton
 
 class GridDialog(QDialog):
-    # def __init__(self):
-    #     super().__init__()
+    def __init__(self):
+        super().__init__()
 
-    #     self.setWindowTitle('Set grid size')
+        self.setWindowTitle('Set grid size')
 
     #     # Create a label and line edit for the number of rows
     #     rows_label = QLabel('Number of Rows:')
@@ -36,32 +36,32 @@ class GridDialog(QDialog):
     #     self.setLayout(layout)
 
     # def submit_grid_size(self):
-        rows = int(self.rows_line_edit.text())
-        cols = int(self.cols_line_edit.text())
+    #     rows = int(self.rows_line_edit.text())
+    #     cols = int(self.cols_line_edit.text())
 
-        # Check if the inputs are even numbers
-        if rows % 2 != 0 or cols % 2 != 0:
-            error_dialog = QDialog()
-            error_dialog.setWindowTitle('Error')
-            error_label = QLabel('Rows and columns must be even numbers')
-            error_layout = QGridLayout()
-            error_layout.addWidget(error_label, 0, 0)
-            error_dialog.setLayout(error_layout)
-            error_dialog.exec()
-        else:
-            # Create a new dialog to display the grid
-            grid_dialog = QDialog()
-            grid_dialog.setWindowTitle('Grid')
+    #     # Check if the inputs are even numbers
+    #     if rows % 2 != 0 or cols % 2 != 0:
+    #         error_dialog = QDialog()
+    #         error_dialog.setWindowTitle('Error')
+    #         error_label = QLabel('Rows and columns must be even numbers')
+    #         error_layout = QGridLayout()
+    #         error_layout.addWidget(error_label, 0, 0)
+    #         error_dialog.setLayout(error_layout)
+    #         error_dialog.exec()
+    #     else:
+    #         # Create a new dialog to display the grid
+    #         grid_dialog = QDialog()
+    #         grid_dialog.setWindowTitle('Grid')
 
-            # Create a grid layout with the specified number of rows and columns
-            grid_layout = QGridLayout()
-            for i in range(rows):
-                for j in range(cols):
-                    label = QLabel(f'({i},{j})')
-                    grid_layout.addWidget(label, i, j)
+    #         # Create a grid layout with the specified number of rows and columns
+    #         grid_layout = QGridLayout()
+    #         for i in range(rows):
+    #             for j in range(cols):
+    #                 label = QLabel(f'({i},{j})')
+    #                 grid_layout.addWidget(label, i, j)
 
-            grid_dialog.setLayout(grid_layout)
-            grid_dialog.exec()
+    #         grid_dialog.setLayout(grid_layout)
+    #         grid_dialog.exec()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
