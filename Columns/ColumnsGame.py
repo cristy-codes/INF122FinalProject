1ff58fcb73ff29d9784bf1329504ff7b346be4ed
Nico <70676144+nplatt2@users.noreply.github.com>
2023-02-28 17:52:40 -0800

    Added Documentation
    Added Documentation

--- Columns/ColumnsGame.py
+++ Columns/ColumnsGame.py
@@ -9,14 +9,16 @@ from Columns.ColumnsTurn import ColumnsTurn
 
 class ColumnsGame(QDialog):
     def __init__(self, rows=24, cols=6, maxTime=100):
+        #Initilizes attributtes
         super().__init__()
         self.conditions = ColumnsConditions(maxTime)
-        self.columnsTurn = ColumnsTurn()
-        self.controller = ColumnsController(self.conditions, self.columnsTurn)
-        self.board = ColumnsBoard(rows, cols, self.controller.handler)
-        self.controller.setBoard(self.board)
-        self.controller.start()
+        self.columnsTurn = ColumnsTurn()    #Instanciate a Game Turn
+        self.controller = ColumnsController(self.conditions, self.columnsTurn) #Instanciate a Controller
+        self.board = ColumnsBoard(rows, cols, self.controller.handler) #Create a Board with the specified dimensions
+        self.controller.setBoard(self.board) #give the controller object the board
+        self.controller.start() #start the controller, which starts the game
 
+        #sets layout of the window
         self.setLayout(self.board)
         self.setWindowTitle("Columns")
         self.setGeometry(50, 50, 200, 200)
