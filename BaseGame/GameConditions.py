9c0d3cf402e19dbc64186cbe39d7b0da4da164c6
MichaelKND <dinhm1@uci.edu>
2023-03-11 16:53:19 -0800

    Changed base class signatures to match and updated scores
    Max Score can only be 125 (80 + 45 seconds). Previous score calculation was bugged.

--- BaseGame/GameConditions.py
+++ BaseGame/GameConditions.py
@@ -1,28 +1,34 @@
 from abc import ABC, abstractmethod
 
+from BaseGame import Board
+from BaseGame import Score
+from BaseGame import Tile
+
+
 class GameConditions():
     def __init__(self, maxTime):
         self.maxGameTime = maxTime
-    
+
     # setter for game time
-    def setMaxGameTime(self, maxTime:int):
+    def setMaxGameTime(self, maxTime: int):
         self.maxGameTime = maxTime
 
     # getter for game time
     def getMaxGameTime(self):
-        return self.maxGameTime 
+        return self.maxGameTime
 
-    # function that determines whether or not the game is still active or over
+        # function that determines whether or not the game is still active or over
+
     @abstractmethod
-    def gameOverCondition(self):
+    def gameOverCondition(self, board: Board):
         pass
-    
+
     # establishes what happens when a tile is clicked
     @abstractmethod
-    def clickEvent(self):
+    def clickEvent(self, tile: Tile, board: Board):
         pass
-    
+
     # calculates points
     @abstractmethod
-    def pointSystem(self):
+    def pointSystem(self, score: Score, board: Board):
         pass
