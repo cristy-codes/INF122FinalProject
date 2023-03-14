a0a36ae1c7cd295d4420c4c80c8ad9a58bafdf13
MichaelKND <dinhm1@uci.edu>
2023-03-13 17:01:26 -0700

    Revert "Merge pull request #6 from cristy-codes/nico_removal_algorithm"
    This reverts commit dfdbd4ba0b0a4f444fa9618cb0580f2a236370ee, reversing
    changes made to 9c0d3cf402e19dbc64186cbe39d7b0da4da164c6.

--- Columns/ColumnsTurn.py
+++ Columns/ColumnsTurn.py
@@ -10,9 +10,5 @@ class ColumnsTurn(TurnEvent):
         for i in range(3):
             # get a random color out of six colors
             color = random.choice(board.get_colors()[1:7])
-
-            #### USE TO DEBUG 3 same color column drop bug
-            #color = random.choice(board.get_colors()[1:3])
-
             # set ith column tile to color
             board.column[i].setColor(color)
